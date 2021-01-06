ba854fc3d54f07f2daabba5acd0310e3d5596187
Michael Parkin <73506758+mike-parkin@users.noreply.github.com>
2021-01-07 10:22:51 +1300

    scoring gutterball frames

--- score.test.js
+++ score.test.js
@@ -1,3 +1,12 @@
+const score = require('./score')
+
 test('test setup working', () => {
     expect(true).toBeTruthy()
   })
+
+test('scores a gutterball frame', () => {
+const frame = [0, 0]
+const expected = 0
+const actual = score.scoreFrame(frame)
+expect(actual).toBe(expected)
+})
