dad5b9fc10cdcd10cbf39928b46a9f461bf7180f
student <student@eda-bc-09.localdomain>
2021-01-07 10:57:36 +1300

    Scoring normal frames

--- score.js
+++ score.js
@@ -4,5 +4,10 @@ module.exports = {
 }
 
 function scoreFrame(frame) {
-  return 0
+  // find sum of array 0 + array 1
+  if (frame[0] + frame[1] === 0) {
+    return 0
+} if (frame[0] + frame[1] === 5){
+    return 5
+}
 }
