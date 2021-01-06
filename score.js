165b3eaaf439c04d4c37a0ed791588acc8153874
Jack Bennett <73858124+j-ack-bennett@users.noreply.github.com>
2021-01-07 10:31:32 +1300

    Scoring normal frames

--- score.js
+++ score.js
@@ -1,6 +1,13 @@
 
 function scoreFrame (frame){
-  return 0
+  const numOne = frame[0]
+  const numTwo = frame[1]
+  const sum = numOne + numTwo
+  console.log(numOne, numTwo, sum)
+  if (sum === 0) {
+    return 0
+  }
+  return sum
 }
 
 
