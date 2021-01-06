4a5bd161a155b1dbb6c0db4b77312625371e82f8
Ste-Perry <73620112+Ste-Perry@users.noreply.github.com>
2021-01-07 10:53:19 +1300

    single strike

--- score.js
+++ score.js
@@ -4,16 +4,24 @@ function scoreFrame (frame, nextFrame){
 
   const numOne = frame[0]
   const numTwo = frame[1]
+  const nfOne = nextFrame[0]
+  const nfTwo = nextFrame[1]
   const sum = numOne + numTwo
-
+  const nfSum = nfOne + nfTwo
+  
   if (sum === 0) {
     return 0
   }
 
-  if (sum === 10){
+  else if (sum === 10 && frame[1] !== 0){
 
     return sum + nextFrame[0]
   }
+  else if (sum === 10 && frame[1] === 0){
+    
+    return sum + nfSum
+  }
+console.log(nfOne)
 
   return sum
 
--- score.test.js
+++ score.test.js
@@ -6,15 +6,17 @@ test('test setup working', () => {
 
 test('scores a gutterball frame', () => {
   const frame = [0, 0]
+  const nextFrame = [0,0]
   const expected = 0
-  const actual = score.scoreFrame(frame)
+  const actual = score.scoreFrame(frame, nextFrame)
   expect(actual).toBe(expected)
 })
 
 test('scores a normal frame', () => {
   const frame = [2, 3]
+  const nextFrame = [0,0]
   const expected = 5
-  const actual = score.scoreFrame(frame)
+  const actual = score.scoreFrame(frame, nextFrame)
   expect(actual).toBe(expected)
 })
 
@@ -22,6 +24,14 @@ test('scores a spare frame', () => {
   const frame = [4, 6]
   const nextFrame = [2, 1]
   const expected = 12
+  const actual = score.scoreFrame(frame, nextFrame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a single strike frame', () => {
+  const frame = [10, 0]
+  const nextFrame = [5, 1]
+  const expected = 16
   const actual = score.scoreFrame(frame, nextFrame)
   expect(actual).toBe(expected)
 })
