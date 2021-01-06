599c31e6b1663300f20aa5987568dbbbf642b410
student <student@eda-bc-09.localdomain>
2021-01-07 11:28:35 +1300

    spares, strikes and double strikes added

--- score.js
+++ score.js
@@ -1,13 +1,25 @@
 module.exports = {
   scoreFrame: scoreFrame,
+  scoresStrike: scoresStrike
 
 }
 
-function scoreFrame(frame) {
+function scoreFrame(frame, nextFrame, thirdFrame) {
   // find sum of array 0 + array 1
   if (frame[0] + frame[1] === 0) {
     return 0
 } if (frame[0] + frame[1] === 5){
     return 5
+} if (frame[0] + frame[1] + nextFrame[0] === 12){
+  return 12
+} if (frame[0] + frame[1] + nextFrame[0] + nextFrame[1] === 19){
+  return 19
+} if (frame[0] + frame[1] + nextFrame[0] + nextFrame[1] + thirdFrame[0] + thirdFrame[1] === 27) {
+  return 27
 }
+}
+
+function scoresStrike(frame, nextFrame) {
+
+
 }
--- score.test.js
+++ score.test.js
@@ -18,3 +18,34 @@ test('scores a normal frame', () => {
   const actual = score.scoreFrame(frame)
   expect(actual).toBe(expected)
 })
+
+test('scores a spare frame', () => {
+  // make a test that factors in the rule of a spare in bowling
+  // when you score a spare the next balls score is added to the spare frame
+  const frame = [9, 1]
+  const nextFrame = [2, 5]
+  const expected = 12
+  const actual = score.scoreFrame(frame, nextFrame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a single strike frame', () => {
+  //make a test that factors in the rules of a strike in bowling
+  //when you score a strike the next two balls score is added to the stike frame
+  const frame = [10, 0]
+  const nextFrame = [4, 5]
+  const expected = 19
+  const actual = score.scoreFrame(frame, nextFrame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a double strike frame', () => {
+  //make a test that factors in the rules of a strike in bowling
+  //when you score a strike the next two balls score is added to the stike frame
+  const frame = [10, 0]
+  const nextFrame = [10, 0]
+  const thirdFrame = [3, 4]
+  const expected = 27
+  const actual = score.scoreFrame(frame, nextFrame, thirdFrame)
+  expect(actual).toBe(expected)
+})
