50b834f2183d4f862e8babbb279cb2574bc382b6
Rosemary-Hawkes <15070793+Rosemary-Hawkes@users.noreply.github.com>
2021-01-10 14:07:31 +1300

    Completed tests

--- score.js
+++ score.js
@@ -1,13 +1,14 @@
 module.exports = {
-  scoreFrame: scoreFrame
+  scoreFrame: scoreFrame,
+  scoreGame: scoreGame
 }
 
 
 
 function scoreFrame (frame, nextFrame, nextNextFrame) {
 
-  //normal frames
-  let score = frame[0] + frame[1]  
+   //normal frames
+   let score = frame[0] + frame[1] 
   //double strike
   if (frame[0] === 10 && nextFrame[0] === 10 && nextNextFrame) {
        score = score + nextFrame[0] + nextNextFrame[0]
@@ -21,4 +22,20 @@ function scoreFrame (frame, nextFrame, nextNextFrame) {
 }
   return score  
 }
+
+
+function scoreGame (results) {
+  
+  let total = 0;
+  // go through entire array, add up, return total
+  for (let i = 0; i < results.length; i++) {
+      if (results[i].length === 3 ) {
+        total = total + results[i][0] + results[i][1] + results[i][2]
+      } else {
+        total = total + scoreFrame (results[i], results[i + 1], results[i + 2])
+      }
+    
+  }
+  return total
+} 
 
--- score.test.js
+++ score.test.js
@@ -5,7 +5,6 @@ test('test setup working', () => {
 })
 
 
-
 test('scores a gutterball frame', () => {
   const frame = [0, 0]
   const expected = 0
@@ -20,6 +19,7 @@ test('scores a normal frame', () => {
   const actual = score.scoreFrame(frame)
   expect(actual).toBe(expected)
 })
+
 
 test('scores a spare frame', () => {
   const frame = [4, 6]
@@ -50,6 +50,29 @@ test('scores a double strike frame', () => {
   const expected = 23
 
   const actual = score.scoreFrame(frame, nextFrame, nextNextFrame)
+
+  expect(actual).toBe(expected)
+})
+
+
+test('scores a game', () => {
+  const frames = [
+    [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4]
+  ] //equals 119 in bowling rules
+  const expected = 119
+
+  const actual = score.scoreGame(frames)
+
+  expect(actual).toBe(expected)
+})
+
+test('scores a spare in the 10th frame', () => {
+  const results = [
+    [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [6, 4, 6]
+  ] //equals 127 in bowling rules
+  const expected = 127
+
+  const actual = score.scoreGame(results)
 
   expect(actual).toBe(expected)
 })
